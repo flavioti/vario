2cc602a1cc7d57565953e59fad3620086e072a47
Flávio Sousa <flaviomarcioti@gmail.com>
2021-10-05 02:47:29 -0300

    decrease vario queue from 10 to 5

--- src/queue.cpp
+++ src/queue.cpp
@@ -2,7 +2,7 @@
 #include "FreeRTOS.h"
 #include "queue.hpp"
 
-QueueHandle_t xQueueVario = xQueueCreate(10, sizeof(float));
+QueueHandle_t xQueueVario = xQueueCreate(5, sizeof(float));
 QueueHandle_t xQueueIngressBarometricData = xQueueCreate(1, sizeof(struct BARO_data *));
 QueueHandle_t xQueueMPU6050 = xQueueCreate(1, sizeof(struct MPU_data *));
 QueueHandle_t xQueueMPU6050Metrics = xQueueCreate(1, sizeof(struct ENCODER_Motion *));
